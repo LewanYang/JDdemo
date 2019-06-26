43f9de9a652873a646baf8a16b30c03a54b70072
lewanyang <3278353995@qq.com>
2019-06-26 10:26:49 +0800

    首页

---
+++ app/js/core/btn.vue
@@ -0,0 +1,28 @@
+<template>
+    <div :class="[btnClass,cname]">
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        cname: {
+            type: String,
+            defalut: '',
+        }
+    },
+    data() {
+        return {
+            btnClass: 'btn',
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import "../../css/element.scss";
+.btn {
+    @include btn;
+}
+</style>
--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,18 +1,42 @@
 <template>
-    <div :class="$style.home">
-        <h1>Home2</h1>
+    <div>
+        <heador></heador>
+        <h-slider></h-slider>
+        <novice></novice>
+        <borrow></borrow>
+        <money></money>
+        <product></product>
+        <life></life>
+        <Footer></Footer>
+        <Navbar></Navbar>
     </div>
 </template>
 
 <script>
+// 这就算是入口文件 层次分明，比较干净
+import Heador from "../public/header.vue"
+import HSlider from "./hslider.vue"
+import Demo from "./demo.vue"
+import Novice from "./novice.vue"
+import Borrow from "./borrow.vue"
+import Money from "./money.vue"
+import Product from "./product.vue"
+import Life from "./life.vue"
+import Footer from "../public/footer.vue"
+import Navbar from "../public/navbar.vue"
 export default {
+    components: {
+        Heador,
+        HSlider,
+        Demo,
+        Novice,
+        Borrow,
+        Money,
+        Product,
+        Life,
+        Footer,
+        Navbar
+    }
 }
 </script>
 
-<style lang="scss" module>
-  @import '../../css/reset.scss';
-  .home{
-    color: red;
-    font-size: 80px;
-  }
-</style>
